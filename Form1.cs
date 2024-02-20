7e4bd8068cd88d6520a3fb83f827aa2ffbd26242
Trong111a <22110445@student.hcmute.edu.vn>
2024-02-20 20:30:08 +0700

    cap nhat nhanh con

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,10 @@ namespace Example
     public partial class Form1 : Form
     {
         // cập nhật lần 1
+        // cập nhật nhánh con
+        // cập nhật nhánh con lần 2
+        // cập nhật nhánh con lần 3
+        // cập nhật nhánh con lần 4
         public Form1()
         {
             InitializeComponent();
